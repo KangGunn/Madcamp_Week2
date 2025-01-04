08c8ca63bb990fa2ca5a32e9dcdd3e24ab835aab
yhtlaeh <102238524+KangGunn@users.noreply.github.com>
2025-01-05 03:10:33 +0900

    SearchActivity

--- app/src/main/java/com/example/re/CourseAdapter.java
+++ app/src/main/java/com/example/re/CourseAdapter.java
@@ -27,17 +27,26 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
     @Override
     public CourseViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.course_item, parent, false);
+                .inflate(R.layout.item_course_card, parent, false);
         return new CourseViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CourseViewHolder holder, int position) {
         CourseResponse course = courses.get(position);
-        holder.tvCourseName.setText(course.getCourseName());
+        holder.tvCourseNameAndCode.setText(course.getCourseName() + "(" + course.getCourseCode() + ")");
         holder.tvDepartment.setText(course.getDepartment());
         holder.tvCourseType.setText(course.getCourseType());
         holder.tvSubjectType.setText(course.getSubjectType());
+        if (course.getSection() == null) {
+            holder.tvCourseSectionAndProfessor.setText(course.getProfessor());
+        } else {
+            holder.tvCourseSectionAndProfessor.setText("분반 " + course.getSection() + " / " + course.getProfessor());
+        }
+        holder.tvLPC.setText(course.getLpc());
+        holder.tvCapacity.setText("정원 " + course.getCapacity());
+        holder.tvLectureTime.setText(course.getLectureTime());
+        holder.tvLectureRoom.setText(course.getLectureRoom());
     }
 
     @Override
@@ -46,13 +55,19 @@ public class CourseAdapter extends RecyclerView.Adapter<CourseAdapter.CourseView
     }
 
     static class CourseViewHolder extends RecyclerView.ViewHolder {
-        TextView tvCourseName, tvDepartment, tvCourseType, tvSubjectType;
+        TextView tvCourseNameAndCode, tvDepartment, tvCourseType, tvSubjectType, tvCourseSectionAndProfessor,
+                 tvLPC, tvCapacity, tvLectureTime, tvLectureRoom;
         public CourseViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvCourseName = itemView.findViewById(R.id.tvCourseName);
-            tvDepartment = itemView.findViewById(R.id.tvDepartment);
-            tvCourseType = itemView.findViewById(R.id.tvCourseType);
-            tvSubjectType = itemView.findViewById(R.id.tvSubjectType);
+            tvCourseNameAndCode = itemView.findViewById(R.id.card_course_name_and_code);
+            tvDepartment = itemView.findViewById(R.id.card_department);
+            tvCourseType = itemView.findViewById(R.id.card_course_type);
+            tvSubjectType = itemView.findViewById(R.id.card_subject_type);
+            tvCourseSectionAndProfessor = itemView.findViewById(R.id.card_section_and_professor);
+            tvLPC = itemView.findViewById((R.id.card_lpc));
+            tvCapacity = itemView.findViewById(R.id.card_capacity);
+            tvLectureTime = itemView.findViewById(R.id.card_lecture_time);
+            tvLectureRoom = itemView.findViewById(R.id.card_lecture_room);
         }
     }
 }
--- app/src/main/java/com/example/re/SearchActivity.java
+++ app/src/main/java/com/example/re/SearchActivity.java
@@ -1,4 +1,95 @@
 package com.example.re;
 
-public class SearchActivity {
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class SearchActivity extends AppCompatActivity {
+
+    public SearchActivity() {
+        //
+    }
+
+    private RecyclerView recyclerView;
+    private CourseAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_search); // 레이아웃 파일 이름 변경
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        recyclerView = findViewById(R.id.course_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new CourseAdapter(new ArrayList<>());
+        recyclerView.setAdapter(adapter);
+
+        ApiService apiService = RetrofitClient.getApiService();
+
+        // CourseRequest 생성 방법 개선 (선택 사항)
+        // Map<String, String> filters = new HashMap<>();
+        // filters.put("department", "전산학부");
+        // filters.put("course_type", "학사과정");
+        // filters.put("subject_type", "전공필수");
+        // List<String> displayColumns = Arrays.asList("학과", "교과목코드", "교과목명", "담당교수", "강:실:학");
+        // CourseRequest request = new CourseRequest(filters, displayColumns);
+
+        // 현재 CourseRequest 생성 방식 유지
+        CourseRequest request = new CourseRequest(
+                "전산학부",
+                "학사과정",
+                "기초필수",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                null,
+                "",
+                "",
+                ""
+        );
+
+        apiService.getFilteredCourses(request).enqueue(new Callback<List<CourseResponse>>() {
+            @Override
+            public void onResponse(Call<List<CourseResponse>> call, Response<List<CourseResponse>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<CourseResponse> courses = response.body();
+                    adapter.updateData(courses);
+                } else {
+                    Log.e("API error", "Response failed with code: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CourseResponse>> call, Throwable t) {
+                Log.e("API Error", "API call failed: " + t.getMessage());
+            }
+        });
+
+    }
 }
